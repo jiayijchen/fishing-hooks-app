5f28a37622b696995e10529e5d97756bceb4f5fe
c.jiayichen <c.jiayichen@gmail.com>
2021-10-29 10:02:56 -0400

    fixed product page button

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -21,22 +21,18 @@ export default function ProductPage(props) {
 
   return (
     <Container fluid className="font-2 mx-0 bg-dark vh-100">
-      <Button onClick={handleBack} variant="link">Back to Products</Button>
-      <Card className="">
+      <Button onClick={handleBack} variant="link" className="text-light">Back to Products</Button>
+      <Card className="border border-dark mx-2">
         <Row>
           <Col>
             <Card.Img variant="top" src={currentProduct?.image} />
-            <Row>
-              <Card.Img variant="top" src={currentProduct?.image} />
-              
-            </Row>
           </Col>
           <Col>
             <Card.Body className="pt-5">
               <div className="h1 fw-bold">{currentProduct?.name}</div>
               <Card.Title className="h1 text-success">${currentProduct?.price + 0.99}</Card.Title>
               <Card.Text>{currentProduct?.description}</Card.Text>
-              <Button variant="success" className="position-absolute bottom-0 end-0 m-3">Add to Cart</Button>
+              <Button variant="success" className="position-absolute bottom-0 end-0 m-3" onClick={() => props.addToCart(currentProduct)}>Add to Cart</Button>
             </Card.Body>
           </Col>
         </Row>
